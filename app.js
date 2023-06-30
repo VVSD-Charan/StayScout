6572030d7b2a014d070ff9fa9b35c822ed230f87
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-30 19:39:59 +0530

    Added catchAsync to avoid server crashes on error.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Room = require('./models/rooms');
 
@@ -33,50 +34,47 @@ app.use(methodOverride('_method'));
 app.get('/',(req , res)=>{
     res.render('home');
 });
-app.get('/rooms',async (req , res) => {
+app.get('/rooms',catchAsync(async (req , res) => {
     const rooms = await Room.find();
     res.render('rooms/index',{rooms});
-});
+}));
 app.get('/rooms/new',(req , res)=>{
     res.render('rooms/new');
 });
-app.get('/rooms/:id',async(req , res)=>{
+app.get('/rooms/:id',catchAsync(async(req , res)=>{
     const room = await Room.findById(req.params.id);
     res.render('rooms/show',{room});
-});
-app.get('/rooms/:id/edit',async (req , res)=>{
+}));
+app.get('/rooms/:id/edit',catchAsync(async (req , res)=>{
     const room = await Room.findById(req.params.id);
     res.render('rooms/edit',{room});
-});
+}));
 
 
 //Handle post requests
-app.post('/rooms',async(req , res , next)=>{
-    try{
-        const room = new Room(req.body.room);
-        await room.save();
-        res.redirect(`/rooms/${room._id}`);
-    }catch(e){
-        next(e);
-    }
-});
+app.post('/rooms',catchAsync(async(req , res , next)=>{
+    
+    const room = new Room(req.body.room);
+    await room.save();
+    res.redirect(`/rooms/${room._id}`);
+}));
 
 
 //Handle put requests
-app.put('/rooms/:id',async(req , res)=>{
+app.put('/rooms/:id',catchAsync(async(req , res)=>{
     const {id}=req.params;
     const {title,location,image,price,description} = req.body.room;
 
     await Room.findByIdAndUpdate(id,{title,location,image,description,price},{new:true});
     res.redirect(`/rooms/${id}`);
-});
+}));
 
 // Handle delete requests
-app.delete('/rooms/:id',async ( req , res) =>{
+app.delete('/rooms/:id',catchAsync(async ( req , res) =>{
     const {id} = req.params;
     await Room.findByIdAndDelete(id);
     res.redirect('/rooms');
-});
+}));
 
 //Validate forms
 app.use((err , req , res , next)=>{
