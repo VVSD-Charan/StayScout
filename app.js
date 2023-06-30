b3c02dd3f015e63e1f0ebf0a23b4ec7c834949ce
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-30 19:11:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Room = require('./models/rooms');
+
+//Connecting to DB
+mongoose.connect('mongodb://127.0.0.1:27017/StayScout',{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+});
+
+//Check if connection is successful
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"Connection error"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+const app = express();
+
+//Set paths and view engine
+app.engine('ejs',ejsMate);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+
+//Handle get requests
+app.get('/',(req , res)=>{
+    res.render('home');
+});
+app.get('/rooms',async (req , res) => {
+    const rooms = await Room.find();
+    res.render('rooms/index',{rooms});
+});
+app.get('/rooms/new',(req , res)=>{
+    res.render('rooms/new');
+});
+app.get('/rooms/:id',async(req , res)=>{
+    const room = await Room.findById(req.params.id);
+    res.render('rooms/show',{room});
+});
+app.get('/rooms/:id/edit',async (req , res)=>{
+    const room = await Room.findById(req.params.id);
+    res.render('rooms/edit',{room});
+});
+
+
+//Handle post requests
+app.post('/rooms',async(req , res)=>{
+    const room = new Room(req.body.room);
+    await room.save();
+    res.redirect(`/rooms/${room._id}`);
+});
+
+
+//Handle put requests
+app.put('/rooms/:id',async(req , res)=>{
+    const {id}=req.params;
+    const {title,location} = req.body.room;
+
+    await Room.findByIdAndUpdate(id,{title,location},{new:true});
+    res.redirect(`/rooms/${id}`);
+});
+
+// Handle delete requests
+app.delete('/rooms/:id',async ( req , res) =>{
+    const {id} = req.params;
+    await Room.findByIdAndDelete(id);
+    res.redirect('/rooms');
+});
+
+//Start server
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log("Serving on port 3000");
+});
---
+++ seeds/index.js
@@ -0,0 +1,43 @@
+//Connecting to DB
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const {places,descriptors} = require('./seedHelpers');
+const Room = require('../models/rooms');
+
+mongoose.connect('mongodb://127.0.0.1:27017/StayScout',{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+});
+
+//Check if connection is successful
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"Connection error"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+const sample = (array) => {
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+const seedDB = async () =>{
+    await Room.deleteMany({});
+    
+    for(let i=0;i<50;i++){
+        const random1000 = Math.floor(Math.random()*1000);
+
+        const room = new Room({
+            location : `${cities[random1000].city}, ${cities[random1000].state}`,
+            title : `${sample(descriptors)} , ${sample(places)}`,
+            image : 'https://source.unsplash.com/collection/483251',
+            description : "Discover a wide range of available room rentals across diverse locations on our website.",
+            price : 500
+        })
+
+        await room.save();
+    }
+};
+
+seedDB().then(()=>{
+    mongoose.connection.close();
+});
