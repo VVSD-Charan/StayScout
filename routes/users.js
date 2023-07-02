841b64f2a97bd42d54d00a5a8b81ad27a7af160a
VVSD Charan <vvsdcharan2001@gmail.com>
2023-07-02 13:27:47 +0530

    Added register user functionality and used flash

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 //Handle get requests
@@ -8,8 +9,21 @@ router.get('/register',(req , res)=>{
 });
 
 //Handle post requests
-router.post('/register',async(req , res) =>{
-    res.send(req.body);
-});
+router.post('/register',catchAsync(async(req , res) =>{
+   
+    try{
+        const {email,username,password}=req.body;
+        const user = new User({email,username});
+        const registeredUser = await User.register(user,password);
+
+        console.log(registeredUser);
+        req.flash('success','Welcome to StayScout !');
+    
+        res.redirect('/rooms');
+    }catch(err){
+        req.flash('error',err.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports=router;
