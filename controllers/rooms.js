40c3a0c968239940ab0860ee6445a20086ed3035
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-07-20 14:45:24 +0530

    Fixed all rooms-controllers

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -9,7 +9,15 @@ module.exports.renderNewForm = (req , res)=>{
     res.render('rooms/new');
 };
 
-module.exports.createRoom = async(req , res)=>{
+module.exports.createRoom = async(req , res , next)=>{
+    const room = new Room(req.body.room);
+    room.author = req.user._id;
+    await room.save();
+    req.flash('success','Successfully made a new rental house');
+    res.redirect(`/rooms/${room._id}`);
+};
+
+module.exports.showRoom = async(req , res)=>{
     const room = await Room.findById(req.params.id).populate({
         path : 'reviews',
         populate : {
@@ -22,4 +30,33 @@ module.exports.createRoom = async(req , res)=>{
         return res.redirect('/rooms');
     }
     res.render('rooms/show',{room});
+};
+
+module.exports.renderEditForm = async (req , res)=>{
+    const room = await Room.findById(req.params.id);
+
+    if(!room){
+        req.flash('error','Cannot find that room');
+        return res.redirect('/rooms');
+    }
+    res.render('rooms/edit',{room});
+};
+
+module.exports.updateRoom = async(req , res)=>{
+    const {id}=req.params;
+    const room = await Room.findById(id);
+
+    const {title,location,image,price,description} = req.body.room;
+
+    await Room.findByIdAndUpdate(id,{title,location,image,description,price},{new:true});
+    req.flash('success','Successfully updated room details!');
+    res.redirect(`/rooms/${id}`);
+};
+
+module.exports.deleteRoom = async ( req , res) =>{
+    const {id} = req.params;
+    await Room.findByIdAndDelete(id);
+
+    req.flash('success','Room has been deleted successfully');
+    res.redirect('/rooms');
 };
--- routes/rooms.js
+++ routes/rooms.js
@@ -5,52 +5,27 @@ const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn,isAuthor,validateRoom} = require('../middleware');
 const Room = require('../models/rooms');
 
-//Handling show rooms get request
+
+//Render all rooms
 router.get('/',catchAsync(rooms.index)); 
-//Login form
+//Render new room Form
 router.get('/new', isLoggedIn ,rooms.renderNewForm);
-//Handling get room requests
-router.get('/:id',catchAsync(rooms.createRoom));
+//Render room data
+router.get('/:id',catchAsync(rooms.showRoom));
+//Render Edit Form
+router.get('/:id/edit',isLoggedIn,isAuthor,catchAsync(rooms.renderEditForm));
 
-router.get('/:id/edit',isLoggedIn,isAuthor,catchAsync(async (req , res)=>{
-    const room = await Room.findById(req.params.id);
-
-    if(!room){
-        req.flash('error','Cannot find that room');
-        return res.redirect('/rooms');
-    }
-    res.render('rooms/edit',{room});
-}));
 
 //Handle post requests
-router.post('/',isLoggedIn,validateRoom,catchAsync(async(req , res , next)=>{
-    const room = new Room(req.body.room);
-    room.author = req.user._id;
-    await room.save();
-    req.flash('success','Successfully made a new rental house');
-    res.redirect(`/rooms/${room._id}`);
-}));
+//Handling create room post request
+router.post('/',isLoggedIn,validateRoom,catchAsync(rooms.createRoom));
 
 //Handle put requests
-router.put('/:id',isLoggedIn, isAuthor , validateRoom ,catchAsync(async(req , res)=>{
-    const {id}=req.params;
-    const room = await Room.findById(id);
-
-    const {title,location,image,price,description} = req.body.room;
-
-    await Room.findByIdAndUpdate(id,{title,location,image,description,price},{new:true});
-    req.flash('success','Successfully updated room details!');
-    res.redirect(`/rooms/${id}`);
-}));
+router.put('/:id',isLoggedIn, isAuthor , validateRoom ,catchAsync(rooms.updateRoom));
 
 //Handle delete requests
-router.delete('/:id',isLoggedIn,isAuthor ,catchAsync(async ( req , res) =>{
-    const {id} = req.params;
-    await Room.findByIdAndDelete(id);
-
-    req.flash('success','Room has been deleted successfully');
-    res.redirect('/rooms');
-}));
+//Delete room
+router.delete('/:id',isLoggedIn,isAuthor ,catchAsync(rooms.deleteRoom));
 
 module.exports = router;
 
