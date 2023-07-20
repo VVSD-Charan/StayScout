b080fc60f00e1fc02d3a88d73f47b6a43239fe3d
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-07-20 15:20:38 +0530

    Added user controller

--- routes/users.js
+++ routes/users.js
@@ -4,56 +4,23 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const {storeReturnTo} =require('../middleware');
 const passport = require('passport');
+const users = require('../controllers/users');
 
 
-// --------------------------- User Register ---------
 //Handle get requests
-router.get('/register',(req , res)=>{
-    res.render('users/register');
-});
+//GET register page
+router.get('/register',users.renderRegister);
 
 //Handle post requests
-router.post('/register',catchAsync(async(req , res) =>{
-   
-    try{
-        const {email,username,password}=req.body;
-        const user = new User({email,username});
-        const registeredUser = await User.register(user,password);
+//Register new user
+router.post('/register',catchAsync(users.register));
 
-        req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }else{
-                req.flash('success','Welcome to StayScout !');
-                res.redirect('/rooms');
-            }
-        });
-    }catch(err){
-        req.flash('error',err.message);
-        res.redirect('register');
-    }
-}));
+//GET login page
+router.get('/login', users.renderLogin);
 
-//------------------ User login ------------- 
+//Login validation
+router.post('/login',storeReturnTo,passport.authenticate('local', {failureFlash : true, failureRedirect : '/login'}) , users.login);
 
-router.get('/login',(req , res)=>{
-    res.render('users/login');
-});
-
-router.post('/login',storeReturnTo,passport.authenticate('local', {failureFlash : true, failureRedirect : '/login'}) ,(req , res)=>{
-    req.flash('success','Welcome back!');
-    const redirectUrl = res.locals.returnTo || '/rooms';
-    res.redirect(redirectUrl);
-});
-
-router.get('/logout',(req , res , next)=>{
-    req.logOut(function(err){
-        if(err){
-            return next(err);
-        }else{
-            req.flash('success','Logged out successfully');
-            res.redirect('/rooms');
-        }
-    });
-});
+//Logout user
+router.get('/logout',users.logout);
 module.exports=router;
