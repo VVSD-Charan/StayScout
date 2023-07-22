8a22117b3d7389d901d150da5038dcbabe4cc6fc
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-07-22 09:27:36 +0530

    Added pin to map which locates room location onmap

--- public/javascripts/showPageMaps.js
+++ public/javascripts/showPageMaps.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [ 79.0821,21.1498], // starting position [lng, lat]
-    zoom: 6, // starting zoom
+    center: room.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(room.geometry.coordinates)
+    .addTo(map)
