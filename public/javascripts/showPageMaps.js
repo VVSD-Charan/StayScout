26f16bd682d406620965c0dc9124c5fcd595f706
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-07-22 10:45:17 +0530

    Added popup to map to display room name onclick

--- public/javascripts/showPageMaps.js
+++ public/javascripts/showPageMaps.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: room.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 12, // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(room.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset : 25})
+            .setHTML(
+                `<h5>${room.title}</h5>`
+            )
+    )
     .addTo(map)
